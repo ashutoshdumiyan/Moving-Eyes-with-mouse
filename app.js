793422f64fe1c29c58dfa31b3084a52be60b800b
Ashutosh Dumiyan <33063623+ashutoshdumiyan@users.noreply.github.com>
2019-07-07 18:37:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const eyeBall = document.querySelectorAll('.eye-ball');
+// console.log(eyeBall);
+document.addEventListener('mousemove', function () {
+    let x = event.clientX * 100 / window.innerWidth + "%";
+    let y = event.clientY * 100 / window.innerHeight + "%";
+    console.log(x + y);
+    for (let i = 0; i < eyeBall.length; i++) {
+        eyeBall[i].style.top = y;
+        eyeBall[i].style.left = x;
+        eyeBall[i].style.transform = "translate(-" + x + ",-" + y + ")";
+    }
+});
